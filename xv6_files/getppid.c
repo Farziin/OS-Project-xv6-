fcf0c52c8158c26aac5270386e025b1458b06275
hossein <farzin.heidary@yahoo.com>
2017-01-12 22:45:44 +0330

    files added

---
+++ xv6_files/getppid.c
@@ -0,0 +1,11 @@
+//
+// Created by hossein on 12/5/16.
+//
+
+#include "user.h"
+
+int main(int argc, char *argv[]){
+    printf(1, "PPID of this process is:%d\n", getppid());
+    exit();
+    return 1;
+}
